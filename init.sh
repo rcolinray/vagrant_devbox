5367795665095fb88a2b9656f8aa48f3d7c09579
Colin Ray <r.colinray@gmail.com>
2016-01-22 13:05:42 -0600

    Use docker to prompt user for quay.io login credentials

--- init.sh
+++ init.sh
@@ -10,11 +10,7 @@ cd taccs
 docker &>/dev/null
 if ! [ $? -eq 0 ]; then
 	sudo ./taccs/tools/taccs_ctl/taccs_ctl.py bootstrap --no-admin-user
-	echo "Please enter your quay.io credentials."
-	read -p "Username: " quay_user
-	read -s -p "Password: " quay_pass
-	read -p "Email: " quay_email
-	sudo docker login -u $quay_user -p $quay_pass -e $quay_email quay.io
+	sudo docker login quay.io
 fi
 
 # Start containers
