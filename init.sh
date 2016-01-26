1b33212745a0b6a7df98bc637a3145053d8a8479
Colin Ray <rcolinray@users.noreply.github.com>
2016-01-26 10:07:31 -0600

    Add comment about why init.sh is used.

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+# This script performs set up routines that are tricky to do through a Vagrant provisioner. 
+# SSH forwarding does not work as one would expect when provisioning, and there is currently
+# no support for prompting the user for input (e.g., for credentials).
+
 # Checkout TACCS source
 if ! [ -d taccs ]; then
 	git clone git@github.com:priority5/taccs.git
